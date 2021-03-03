f7cc145920cd9d5a1e2c799fc1383b3441cf3e36
Chey Rose <cheyroseflammer@gmail.com>
2021-03-03 13:35:40 -0800

    easy http library done

--- easyHttp/script.js
+++ easyHttp/script.js
@@ -1,6 +1,7 @@
+// eslint-disable-next-line no-undef
 const http = new easyHTTP();
 
-// Get posts
+// Get posts/retrieve data
 
 http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
   if (err) {
@@ -9,3 +10,59 @@ http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
     console.log(posts);
   }
 });
+
+// Get single post
+
+http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, post) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+});
+
+// Make post request
+
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post body',
+};
+// Create post
+http.post(
+  'https://jsonplaceholder.typicode.com/posts',
+  data,
+  function (err, post) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(post);
+    }
+  }
+);
+
+// Make PUT request/updating data
+
+http.put(
+  'https://jsonplaceholder.typicode.com/posts/1',
+  data,
+  function (err, post) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(post);
+    }
+  }
+);
+
+// Make DELETE request/deleting data
+
+http.delete(
+  'https://jsonplaceholder.typicode.com/posts/1',
+  function (err, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(response);
+    }
+  }
+);
